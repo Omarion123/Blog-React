1319d002b679449e5cf1075004ef2304622f4254
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 <anzymen@gmail.com>
2023-10-12 21:17:32 +0200

    Working on Contact us

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,14 +1,29 @@
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillMessage } from "react-icons/ai";
+import { BsFillSendFill } from "react-icons/bs";
+
 const Contactus = () => {
   return (
     <div className="contactus">
-      <div className="contact-head">
-        <h3>About us</h3>
-      </div>
-      <form>
-        <div className="email">
-          <input type="email" required />
+      <div className="contact-content">
+        <div className="contact-head">
+          <h3>Contact us</h3>
         </div>
-      </form>
+        <form>
+          <div className="contact-email">
+            <AiOutlineMail className="contact-email-icon" />
+            <input type="email" placeholder="Enter email here..." required />
+          </div>
+          <div className="contact-message">
+            <AiFillMessage className="contact-message-icon" />
+            <input type="text" required placeholder="Enter message here..." />
+          </div>
+          <div className="contact-button">
+            <button className="contact-button-send">Send</button>
+            <BsFillSendFill className="contact-button-icon" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
